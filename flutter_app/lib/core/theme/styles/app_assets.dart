810e855255cb09167580f89ca2ebf470340c9fce
mohaaHeiba <mohamed222101223@ksiu.edu.eg>
2026-08-24 20:51:22 +0300

    fix: update asset paths and color values for consistency across themes

--- flutter_app/lib/core/theme/styles/app_assets.dart
+++ flutter_app/lib/core/theme/styles/app_assets.dart
@@ -1,20 +1,19 @@
-import 'package:tara_car/core/constants/assets_constants.dart';
+import 'package:hope_app/core/constants/assets_constants.dart';
 
 abstract class AppImages {
-  static const String homeLogo = Assets.assetsLogosLightLogoHomePageAsPn;
-  static const String car = Assets.assetsImagesCarLight;
-  static const String policeCar = Assets.assetsImagesPoliceCarLight;
-  static const String rocket = Assets.assetsImagesRocketLight;
+  static const String homeLogo = "Assets.assetsLogosLightLogoHomePageAsPn;";
+  static const String car = "Assets.assetsImagesCarLight;";
+  static const String policeCar = "Assets.assetsImagesPoliceCarLight;";
+  static const String rocket = "Assets.assetsImagesRocketLight;";
   static const String emptyNotification =
-      Assets.assetsImagesEmptyNotificationLight;
+      "Assets.assetsImagesEmptyNotificationLight;";
 }
 
 abstract class AppDarkImages {
-  static const String homeLogo = Assets.assetsLogosDarkLogoHomePageAsPn;
-  static const String car = Assets.assetsImagesCarDark;
-  static const String policeCar = Assets.assetsImagesPoliceCarDark;
-  static const String rocket = Assets.assetsImagesRocketDark;
-
-  static const String emptyNotification =
-      Assets.assetsImagesEmptyNotificationDark;
+  static final String homeLogo = "Assets.assetsLogosDarkLogoHomePageAsPn;";
+  static final String car = "Assets.assetsImagesCarDark;";
+  static final String policeCar = "Assets.assetsImagesPoliceCarDark;";
+  static final String rocket = "Assets.assetsImagesRocketDark;";
+  static final String emptyNotification =
+      "Assets.assetsImagesEmptyNotificationDark;";
 }
