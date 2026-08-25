2b31cd250733817f490a567d8ebef335b48273d4
mohaaHeiba <mohamed222101223@ksiu.edu.eg>
2026-08-25 16:34:36 +0300

    refactor: clean up color definitions and update theme extension for improved readability

--- flutter_app/lib/core/theme/styles/app_colors.dart
+++ flutter_app/lib/core/theme/styles/app_colors.dart
@@ -2,136 +2,62 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const Color textBlack = Color.fromRGBO(28, 28, 28, 1);
-  static const Color secondaryColor = Color(0xff357185);
   static const Color whiteColor = Color(0xffffffff);
   static const Color blackColor = Color(0xff000000);
-  static const Color textDefaultPrimaryColor = Color(0xff022D23);
-  static const Color textDefaultBlackColor = Color(0xff181818);
-  static const Color textSecondaryColor = Color(0xffABABAB);
-  static const Color textStrongColor = Color(0xffA6A6A6);
   static const Color textExtraStrongColor = Color(0xff8C8C8C);
   static const Color borderAndDividerColor = Color(0xffE8E9F1);
   static const Color foregroundColor = Color(0xffF7F7F7);
   static const Color textMutedColor = Color(0xffBFBFBF);
   static const Color disabledButtonColor = Color(0xffEDEDED);
-  static const Color cardBordeerColor = Color(0xffCCE2D9);
   static const Color mutedGoldColor = Color(0xffBC9658);
-  static const Color badgeColor = Color.fromARGB(255, 206, 2, 2);
-  static const Color minusButtonColor = Color.fromRGBO(230, 230, 237, 1);
   static const Color redColor = Color(0xffC9372C);
-
-  static const Color secondaryColorMutedGold = Color(0xffEEF3E7);
-  // static const Color secondaryColor = Color(0xff80A852);
-  // static const Color secondaryColor = Color(0xff80A852);
-  static const Color statusBackgroundColor = Color(0xffEDF4FE);
+  //
+  //
   static const Color statusTextColor = Color(0xff2F80ED);
 
   //new colors
+  static const Color navy = Color(0xFF1A4663);
+  static const Color teal = Color(0xFF357185);
+  static const Color sage = Color(0xFF89B189);
+  static const Color offWhite = Color(0xFFE9E8E6);
+  static const Color ink = Color(0xFF1F2933);
+  static const Color muted = Color(0xFF5C6B77);
+
   static const Color scaffoldBackgroundColor = Color(0xffFFFFFF);
   static const Color primaryColor = Color(0xff1A4663);
   static const Color lightLightnessColor = Color(0xffFFFFFF);
   static const Color darkDark = Color(0xff2F3036);
-  static const Color lightDark = Color(0xffD4D6DD);
   static const Color darkLight = Color(0xff71727A);
   static const Color darkLightest = Color(0xff8F9098);
-  static const Color lightMedium = Color(0xffE8E9F1);
-  static const Color naturalLightLight = Color(0xffF8F9FE);
   static const Color textWhite = Color(0xffffffff);
-  static const Color darkDarkest = Color(0xff1F2024);
-  static const Color lightDarkest = Color(0xffC5C6CC);
-  static const Color lightLightest = Color(0xffFFFFFF);
-  static const Color premiumColor = Color(0xffE8C239);
   static const Color darkMedium = Color(0xff494A50);
   static const Color errorDark = Color(0xffED3241);
-  static const Color addCircleBackgroundColor = Color(0xffD6EAE8);
-  static const Color iconVerified = Color(0xff2E80E5);
-  static const Color containerDivider = Color(0xffF2F2F2);
-  static const Color successLight = Color(0xffE7F4E8);
-  static const Color warningLight = Color(0xffFFF4E4);
-  static const Color warningDark = Color(0xffE86339);
   static const Color errorColor = Color(0xffF13637);
-  static const Color carGreen = Color(0xff42B0A6);
-  static const Color carRed = Color(0xffD82828);
-  static const Color carBlue = Color(0xff313FD9);
-  static const Color carGray = Color(0xffA8A8A8);
-  static const Color carBlack = Color(0xff121212);
-  static const Color carYellow = Color(0xffBEBB28);
-  static const Color carPurple = Color(0xff984AD3);
-  static const Color carOrange = Color(0xffEF931C);
-  static const Color greenLight = Color(0xffCAE8E5);
-  static Color dottedColor = const Color(0xff1A4663).withValues(alpha: .1);
-  static const Color dustyTeal = Color(0xff91C6C2);
-  static const Color oxfordBlue = Color(0xff101828);
 }
 
 abstract class AppDarkColors {
   // Dark Theme Colors
-  static Color lightPrimaryColor = const Color(
-    0xffBB86FC,
-  ).withValues(alpha: .05);
   static const Color textBlack = Color(0xffEAEAEA);
   static const Color textHintBold = Color(0xffC1C1C1);
   static const Color textHintLight = Color(0xffA0A0A0);
-  static const Color textHintMiddle = Color(0xff7A7A7A);
   static const Color redColor = Color(0xffCF2D2D);
   static const Color white = Color(0xffffffff);
   static const Color disabledButtonColor = Color(0xff4B4B4B);
   static const Color bordersColor = Color(0xff333333);
-  static const Color darkPrimaryColor = Color(0xff03DAC5);
   static const Color buttomSheetTopColor = Color(0xff455A64);
-  static const Color darkTextGray = Color(0xff757575);
-  static const Color navBarBorderColor = Color(0xff333333);
-  static const Color searchContainerBorderColor = Color(0xff6082CF);
-  static const Color boxOrderColor = Color(0xff1C1C1C);
   static const Color pendingColor = Color(0xffE5B000);
   static const Color completedColor = Color(0xff00C853);
-  static const Color cancelledColor = Color(0xffC80000);
-  static const Color notificationBoxColor = Color(0xff3A3A3A);
-  static const Color unselectedLanguageColor = Color(0xff455A64);
-  static const Color productOutOfStockColor = Color(0xffFFEEEE);
-  static Color unselectedOrderStatusColor = const Color(
-    0xff455A64,
-  ).withValues(alpha: .5);
-  static Color darkModeSwitchColor = const Color(
-    0xff787880,
-  ).withValues(alpha: .16);
 
   //new colors
   static const Color scaffoldBackgroundColor = Color(0xff141414);
   static const Color primaryColor = Color(0xff1A4663);
   static const Color lightLightnessColor = Color(0xffFFFFFF);
   static const Color darkDark = Color(0xffEAEAEA);
-  static const Color lightDark = Color(0xffD4D6DD);
   static const Color darkLight = Color(0xff919191);
   static const Color darkLightest = Color(0xff8F9098);
-  static const Color lightMedium = Color(0xff333333);
-  static const Color naturalLightLight = Color(0xff232323);
   static const Color textWhite = Color(0xffffffff);
-  static const Color darkDarkest = Color(0xffEAEAEA);
-  static const Color lightDarkest = Color(0xffC5C6CC);
-  static const Color lightLightest = Color(0xff232323);
-  static const Color premiumColor = Color(0xffE8C239);
   static const Color darkMedium = Color(0xffEAEAEA);
   static const Color errorDark = Color(0xffED3241);
-  static const Color addCircleBackgroundColor = Color(0xff333333);
-  static const Color iconVerified = Color(0xff2E80E5);
-  static const Color containerDivider = Color(0xff232323);
-  static const Color successLight = Color(0xffE7F4E8);
-  static const Color warningLight = Color(0xffFFF4E4);
-  static const Color warningDark = Color(0xffE86339);
-  static const Color carGreen = Color(0xff42B0A6);
-  static const Color carRed = Color(0xffD82828);
-  static const Color carBlue = Color(0xff313FD9);
-  static const Color carGray = Color(0xffA8A8A8);
-  static const Color carBlack = Color(0xff121212);
-  static const Color carYellow = Color(0xffBEBB28);
-  static const Color carPurple = Color(0xff984AD3);
-  static const Color carOrange = Color(0xffEF931C);
-  static const Color greenLight = Color(0xffCAE8E5);
-  static Color dottedColor = const Color(0xff1A4663).withValues(alpha: .1);
-  static const Color dustyTeal = Color(0xff91C6C2);
-  static const Color oxfordBlue = Color(0xffF9FAFB);
-
 }
 
 
